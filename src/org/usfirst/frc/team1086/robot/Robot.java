1b0ef57f1cc6dbf5cd055613d5c7f37c3ec91177
jnmcd <undeadmule5@gmail.com>
2018-01-26 21:27:49 -0400

    Somewhat working code?

--- src/org/usfirst/frc/team1086/robot/Robot.java
+++ src/org/usfirst/frc/team1086/robot/Robot.java
@@ -7,13 +7,17 @@
 
 package org.usfirst.frc.team1086.robot;
 
+import java.io.File;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -22,6 +26,7 @@ import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.Waypoint;
 import jaci.pathfinder.followers.EncoderFollower;
 import jaci.pathfinder.modifiers.TankModifier;
+
 public class Robot extends TimedRobot {
 	Joystick leftStick, rightStick, auxStick;
 	
@@ -29,6 +34,12 @@ public class Robot extends TimedRobot {
 	EncoderFollower left, right;
 	TalonSRX talonFrontLeft, talonFrontRight, talonBackLeft, talonBackRight;
 	AHRS vmx;
+	
+	PIDController turnController;
+	double turnOutput = 0;
+	
+	double angleDifference = 0;
+	
 	@Override
 	public void robotInit() {
 		talonFrontLeft = new TalonSRX(1);
@@ -50,10 +61,26 @@ public class Robot extends TimedRobot {
 			System.out.println("VMX is null");
 		}
 		
+		
 		//The first int is pidIdx - 0 for primary closed loop, 1 for cascaded closed loop
 		//The second int is timeoutMs - >0 will timeout error if it times out, 0 means dont check at all
 		talonFrontLeft.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
 		talonFrontRight.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
+	
+		turnController = new PIDController(.012, 0, 0.015, new PIDSource() {
+			@Override public void setPIDSourceType(PIDSourceType pidSource) {}
+
+			@Override
+			public PIDSourceType getPIDSourceType() {
+				return PIDSourceType.kDisplacement;
+			}
+
+			@Override
+			public double pidGet() {
+				return angleDifference;
+			}
+			
+		}, v -> turnOutput = v);
 	}
 	
 	public void drive(double drive, double turn, double strafe) {
@@ -76,8 +103,8 @@ public class Robot extends TimedRobot {
 	public void teleopInit() {
 		points = new Waypoint[] {
 				new Waypoint(0, 0, 0),
-				new Waypoint(1, 0, 0)
-				
+				new Waypoint(2, 0, 0),
+				new Waypoint(3, 0, 0),
 		};
 		talonFrontLeft.setSelectedSensorPosition(0, 0, 0);
 		talonFrontRight.setSelectedSensorPosition(0, 0, 0);
@@ -91,65 +118,100 @@ public class Robot extends TimedRobot {
 		SmartDashboard.putNumber("Distance Travelled", getEncDistance());
 		SmartDashboard.putNumber("VMX Angle", vmx.getYaw());
 		
+		
 		if(leftStick.getRawButton(1))
 			drive(-leftStick.getY(), rightStick.getX(), leftStick.getX());
 		else drive(0, 0, 0);
 	
+		if(auxStick.getRawButton(2)) {
+			drive(0, .5, 0);
+		}
+		
 		if(auxStick.getRawButtonPressed(3)) {
-			double dt = 0.05;
-			double maxVelocity = 1.7;
+			double dt = 0.02;
+			double maxVelocity = 2.848;
 			double maxAcceleration = 2.0;
 			double maxJerk = 60.0;
 			Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC,
 					Trajectory.Config.SAMPLES_FAST, dt, maxVelocity, maxAcceleration, maxJerk);
 			Trajectory trajectory = Pathfinder.generate(points, config);
-			
-			double wheelbaseWidth = 0.43; //Distance between the two sides of DT, in meters.
+			double wheelbaseWidth = 0.6; //Distance between the two sides of DT, in meters.
 			TankModifier modifier = new TankModifier(trajectory).modify(wheelbaseWidth);
 			
 			left = new EncoderFollower(modifier.getLeftTrajectory());
 			right = new EncoderFollower(modifier.getRightTrajectory());
 			
 			//configureEncoder(encoderPosition, encoderTickPerRevolution, wheelDiameter)
-			left.configureEncoder((int)(talonFrontLeft.getSelectedSensorPosition(0) * 1.07), 4096, .1524);
-			right.configureEncoder((int)(talonFrontRight.getSelectedSensorPosition(0) * 1.07), 4096, .1524);
+			left.configureEncoder(talonFrontLeft.getSelectedSensorPosition(0), 4096, .1524);
+			right.configureEncoder(talonFrontRight.getSelectedSensorPosition(0), 4096, .1524);
 		
-			double p = 0.1;
+			double p = .4;
 			double i = 0;
 			double d = 0;
 			double velocityInverse = 1/maxVelocity;
-			double accelGain = 0;
+			double accelGain = 1 / maxAcceleration;
 			left.configurePIDVA(p, i, d, velocityInverse, accelGain);
 			right.configurePIDVA(p, i, d, velocityInverse, accelGain);
+			
+			turnController.setSetpoint(0);
+			turnController.setInputRange(-180, 180);
+			turnController.setOutputRange(-2, 2);
+			turnController.setContinuous(true);
+			turnController.enable();
 		}
 		if(auxStick.getRawButton(3)) { 
-			double leftSpeed = left.calculate(talonFrontLeft.getSelectedSensorPosition(0));
-			double rightSpeed = right.calculate(talonFrontRight.getSelectedSensorPosition(0));
-			double gyroHeading = vmx.getAngle();
-			double desiredHeading = Pathfinder.r2d(left.getHeading());
-			SmartDashboard.putNumber("MP Path x", left.getSegment().x);
-			SmartDashboard.putNumber("MP Path position", left.getSegment().position);
-			double angleDifference = Pathfinder.boundHalfDegrees(desiredHeading - gyroHeading);
-			double turn = 0.04 * (-1.0 / 80.0) * angleDifference;
-			SmartDashboard.putNumber("MP Left Speed", leftSpeed);
-			SmartDashboard.putNumber("MP Right Speed", rightSpeed);
-			SmartDashboard.putNumber("MP Turn", turn);
+			if(!left.isFinished() && !right.isFinished()) {
+				double leftSpeed = left.calculate(talonFrontLeft.getSelectedSensorPosition(0)) / 4;
+				double rightSpeed = right.calculate(talonFrontRight.getSelectedSensorPosition(0)) / 4;
+				double gyroHeading = vmx.getAngle();
+				double desiredHeading = Pathfinder.r2d(left.getHeading());
+				angleDifference = Pathfinder.boundHalfDegrees(desiredHeading + gyroHeading);
+				
+				//double turn = .7 * (-1.0 / 80.0) * angleDifference;
+				double turn = turnController.get();
+				SmartDashboard.putNumber("MP Left Speed", leftSpeed);
+				SmartDashboard.putNumber("MP Right Speed", rightSpeed);
+				SmartDashboard.putNumber("MP Turn", turn);
+				
+				
+				//System.out.println("Left speed: " + leftSpeed);
+				//System.out.println("Right speed: " + rightSpeed);
+				System.out.println("Turn: " + turn);
+				System.out.println("MP Target Turn " +  Pathfinder.r2d(left.getHeading()));
+				try {
+
+					System.out.println("MP Setpoint Turn " + Pathfinder.r2d(left.getSegment().heading));
+				} catch(Exception e) {System.out.println("End ");}
+				
+				talonFrontLeft.set(ControlMode.PercentOutput, leftSpeed + turn);
+				talonBackLeft.set(ControlMode.PercentOutput, leftSpeed + turn);
+				talonFrontRight.set(ControlMode.PercentOutput, rightSpeed - turn);
+				talonBackRight.set(ControlMode.PercentOutput, rightSpeed - turn); 
+			}
+			else {
+				talonFrontLeft.set(ControlMode.PercentOutput, 0);
+				talonBackLeft.set(ControlMode.PercentOutput, 0);
+				talonFrontRight.set(ControlMode.PercentOutput, 0);
+				talonBackRight.set(ControlMode.PercentOutput, 0); 
+				turnController.disable();
+			}
+		}
+		
+		if(auxStick.getRawButtonPressed(4)) {
 			
-			talonFrontLeft.set(ControlMode.PercentOutput, leftSpeed + turn);
-			talonBackLeft.set(ControlMode.PercentOutput, leftSpeed + turn);
-			talonFrontRight.set(ControlMode.PercentOutput, rightSpeed - turn);
-			talonBackRight.set(ControlMode.PercentOutput, rightSpeed - turn); 
 		}
 	}
 	
 	public double getLeftDistance() {
 		double d = talonFrontLeft.getSelectedSensorPosition(0) / 4096.0 * 6 * Math.PI;
-		return d + bestFitLine(d);
+		//return d + bestFitLine(d);
+		return d;
 	}
 	
 	public double getRightDistance() {
 		double d = talonFrontRight.getSelectedSensorPosition(0) / 4096.0 * 6 * Math.PI;
-		return d + bestFitLine(d);
+		//return d + bestFitLine(d);
+		return d;
 	}
 	
 	public double bestFitLine(double dist) {
